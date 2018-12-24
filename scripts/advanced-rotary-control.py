57ad93ac684b9c987f4e1b2dbb2627e42bcc6fe4
tplaputta <tobias.plaputta@affinitas.de>
2018-12-24 10:57:55 +0100

    Adds confirmation sounds for skipping tracks

--- scripts/advanced-rotary-control.py
+++ scripts/advanced-rotary-control.py
@@ -24,49 +24,57 @@ import time
 import signal
 from subprocess import check_call
 
+
 def trigger_held(signal, frame):
-   check_call("./scripts/playout_controls.sh -c=shutdown", shell=True)
+  check_call("./scripts/playout_controls.sh -c=shutdown", shell=True)
+
 
 def check_rotation(gpio, level, tick):
-   global encoder_rotate_time, encLevA, encLevB, lastEncPin
-   if level > 1:
-      return
-   if gpio == encPinA:
-      encLevA = level
-   else:
-      encLevB = level
-   if gpio == lastEncPin: # debounce
-      return
-   lastEncPin = gpio
-   signal.alarm(0)
-   encoder_rotate_time = time.time()
-   pressed = not bool(pi.read(3))
-   if gpio == encPinA and level == 1:
-      if encLevB == 1:
-         if pressed:
-            check_call("./scripts/playout_controls.sh -c=playernext", shell=True)
-         else:
-            check_call("./scripts/playout_controls.sh -c=volumeup", shell=True)
-   elif gpio == encPinB and level == 1:
-      if encLevA == 1:
-         if pressed:
-            check_call("./scripts/playout_controls.sh -c=playerprev", shell=True)
-         else:
-            check_call("./scripts/playout_controls.sh -c=volumedown", shell=True)
+  global encoder_rotate_time, encLevA, encLevB, lastEncPin
+  if level > 1:
+    return
+  if gpio == encPinA:
+    encLevA = level
+  else:
+    encLevB = level
+  if gpio == lastEncPin:  # debounce
+    return
+  lastEncPin = gpio
+  signal.alarm(0)
+  encoder_rotate_time = time.time()
+  pressed = not bool(pi.read(3))
+  if gpio == encPinA and level == 1:
+    if encLevB == 1:
+      if pressed:
+        check_call("mpg123 ./misc/sounds/BubblePo-Benjamin-8920_hifi.mp3", shell=True)
+        check_call("./scripts/playout_controls.sh -c=playernext", shell=True)
+      else:
+        check_call("./scripts/playout_controls.sh -c=volumeup", shell=True)
+  elif gpio == encPinB and level == 1:
+    if encLevA == 1:
+      if pressed:
+        check_call("mpg123 ./misc/sounds/Pop-J_Fairba-8421_hifi.mp3", shell=True)
+        check_call("./scripts/playout_controls.sh -c=playerprev", shell=True)
+      else:
+        check_call("./scripts/playout_controls.sh -c=volumedown", shell=True)
+
 
 def trigger_released(gpio, level, tick):
-   global trigger_press_time, encoder_rotate_time
-   signal.alarm(0)
-   if encoder_rotate_time < trigger_press_time:
-      check_call("./scripts/playout_controls.sh -c=playerpause", shell=True)
+  global trigger_press_time, encoder_rotate_time
+  signal.alarm(0)
+  if encoder_rotate_time < trigger_press_time:
+    check_call("./scripts/playout_controls.sh -c=playerpause", shell=True)
+
 
 def trigger_pressed(gpio, level, tick):
-   global trigger_press_time
-   trigger_press_time = time.time()
-   signal.alarm(5)
+  global trigger_press_time
+  trigger_press_time = time.time()
+  signal.alarm(5)
+
 
 def exit_handler(signal, frame):
   exit(0)
+
 
 signal.signal(signal.SIGINT, exit_handler)
 signal.signal(signal.SIGTERM, exit_handler)
@@ -98,11 +106,10 @@ encLevA = pi.read(encPinA)
 encLevB = pi.read(encPinB)
 lastEncPin = None
 
-
 pi.callback(3, pigpio.FALLING_EDGE, trigger_pressed)
 pi.callback(3, pigpio.RISING_EDGE, trigger_released)
 pi.callback(5, pigpio.EITHER_EDGE, check_rotation)
 pi.callback(6, pigpio.EITHER_EDGE, check_rotation)
 
 while True:
-   signal.pause()
+  signal.pause()
